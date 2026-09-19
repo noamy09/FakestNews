560b72e7096595c232cbade7d32cf73664604cfe
Idan Cohen <idan.cohen42@gmail.com>
2026-09-19 18:19:57 +0300

    feat: implement public feed, SSR views, Ajax filters, and infinite scroll

--- routes/router.js
+++ routes/router.js
@@ -5,19 +5,23 @@ const router = express.Router();
 const articlesRoutes = require('./articlesRoutes');
 const commentsRoutes = require('./commentsRoutes');
 const notesRoutes = require('./notesRoutes');
-const permissionsRoutes = require('./permissionsRoutes');
+
+// Temp out till permissions are made
+// const permissionsRoutes = require('./permissionsRoutes');
 const statisticsRoutes = require('./statisticsRoutes');
 const usersRoutes = require('./usersRoutes');
 
 router.use('/articles', articlesRoutes);
 router.use('/comments', commentsRoutes);
 router.use('/notes', notesRoutes);
-router.use('/permissions', permissionsRoutes);
+
+// Temp out till permissions are made
+// router.use('/permissions', permissionsRoutes);
 router.use('/statistics', statisticsRoutes);
 router.use('/users', usersRoutes);
 
 router.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public", "home.html"));
+    res.redirect("/articles/public");
 });
 
 module.exports = router;
