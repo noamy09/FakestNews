6f314e9fcd07d25f2297f1501bd5ed05bd909fc4
Idan Cohen <idan.cohen42@gmail.com>
2026-09-22 01:16:23 +0300

    Refactor: update public feed and routes according to review

--- routes/articlesRoutes.js
+++ routes/articlesRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/articlesController');
+const { handleArticlesFeed, handleSingleArticle } = require('../utils/publicArticlesUtil');
 
-router.get('/', controller.getAll);
-router.get('/:id', controller.getById);
+router.get('/', publicViewController.renderHomePage);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
 router.delete('/:id', controller.delete);
---
+++ routes/permissionsRoutes.js
@@ -0,0 +1,2 @@
+// TEMP FOR SERVER TO RUN
+const express = require('express'); const router = express.Router(); module.exports = router;
